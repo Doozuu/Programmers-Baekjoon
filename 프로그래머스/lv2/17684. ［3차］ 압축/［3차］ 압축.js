2e57762f4a8c80b1973baf4e0ff225fa5ad2a497
LJH <104717341+Doozuu@users.noreply.github.com>
2023-08-20 18:36:44 +0900

    [level 2] Title: [3차] 압축, Time: 0.48 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,39 +1,28 @@
 function solution(msg) {
     let answer = [];
-    let alpabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let dictionary = alpabet.split('');
+    let dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     let i = 0; 
     while(true){
         let cur = msg[i];
         let next = msg[i+1];
         let cur_idx = dictionary.indexOf(cur);
         if(!next){
-            if(cur_idx > -1){
-                answer.push(cur_idx + 1);
-            }
+            if(cur_idx > -1) answer.push(cur_idx + 1);
             return answer;
         }
         let next_idx = dictionary.indexOf(cur + next);
-        if(cur_idx > -1){
-            if(next_idx > -1){
-                while(next_idx > -1){
-                    cur = cur + next;
-                    i++;
-                    next = msg[i+1];
-                    next_idx = dictionary.indexOf(cur + next);
-                }
-                if(next_idx === -1){
-                    cur_idx = dictionary.indexOf(cur);
-                    answer.push(cur_idx + 1); 
-                    dictionary.push(cur + next); 
-                    i++;
-                }
-            }else{
-                answer.push(cur_idx + 1); 
-                dictionary.push(cur + next); 
+        if(next_idx > -1){
+            while(next_idx > -1){
+                cur = cur + next;
                 i++;
+                next = msg[i+1];
+                next_idx = dictionary.indexOf(cur + next);
             }
+            cur_idx = dictionary.indexOf(cur);
         }
+        answer.push(cur_idx + 1); 
+        dictionary.push(cur + next); 
+        i++;
     }
     return answer;
 }
