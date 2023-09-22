de82428d5d8546d416a2ff1f46f25b1b03354887
LJH <104717341+Doozuu@users.noreply.github.com>
2023-09-22 22:08:56 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -6,20 +6,6 @@ function solution(data, col, row_begin, row_end) {
     for(let i=row_begin-1;i<row_end;i++){
         sum_list.push(data[i].reduce((acc,cur) => acc + cur % (i+1),0));
     }
-    // 각 자리수가 다르면 1, 나머지 0, 자리부족하면 0으로 채우기
-    let answer = sum_list.reduce((acc,cur) => {
-        let xor = '';
-        let acc_binary = acc.toString(2);  
-        let cur_binary = cur.toString(2); 
-        if(acc_binary.length < cur_binary.length){
-            acc_binary = '0'.repeat(cur_binary.length - acc_binary.length) + acc_binary;
-        }else if(acc_binary.length > cur_binary.length){
-            cur_binary = '0'.repeat(acc_binary.length - cur_binary.length) + cur_binary;
-        }
-        for(let i=0;i<acc_binary.length;i++){
-            xor += acc_binary[i] !== cur_binary[i] ? '1' : '0';
-        }
-        return xor;
-    },0)
-    return parseInt(answer,2);
+    // 3. XOR 계산
+    return sum_list.reduce((acc,cur) => acc ^ cur,0);
 }
