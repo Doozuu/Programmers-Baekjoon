1f583772f86e897af15cb7e4d899c66dfb2b0408
LJH <104717341+Doozuu@users.noreply.github.com>
2023-09-27 22:22:30 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
@@ -0,0 +1,18 @@
+function solution(cards) {
+    let answer = [];
+    let visited = Array.from({length: cards.length}, () => false);
+    let j = 0;
+    for(let i=0;i<cards.length;i++){
+        let temp = i;
+        if(!visited[cards[i]-1]){
+            while(!visited[temp]){
+                answer[j] = (answer[j] || 0) + 1;
+                visited[temp] = true;
+                temp = cards[temp] - 1;
+            }
+            j++;
+        }
+    }
+    answer.sort((a,b) => b-a);
+    return answer[0] * answer[1] || 0;
+}
