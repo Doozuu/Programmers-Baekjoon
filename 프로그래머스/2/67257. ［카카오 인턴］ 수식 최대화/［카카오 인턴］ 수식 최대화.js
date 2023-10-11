600ec6f29711f97966417f4a74980311ab36fdda
LJH <104717341+Doozuu@users.noreply.github.com>
2023-10-12 03:39:54 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -0,0 +1,41 @@
+function solution(expression) {
+    let operator = expression.replace(/[0-9]/g,'');
+    let distinct_operator = [...new Set(operator.split(''))];
+    function change(num1, num2, op){
+        if(op === '+'){
+            return num1 + num2;
+        }else if(op === '-'){
+            return num1 - num2;
+        }else{
+            return num1 * num2;
+        }
+    }
+    if(distinct_operator.length === 1){
+        let op = distinct_operator[0];
+        let num_list = expression.split(op).map(Number);
+        return Math.abs(num_list.reduce((acc,cur) => change(acc,cur,op)));
+    }else if(distinct_operator.length === 2){
+        let [op1,op2] = distinct_operator;
+        let candidate1 = expression.split(op2).map(el => {
+            return el.split(op1).map(Number).reduce((acc,cur) => change(acc,cur,op1))
+        }).reduce((acc,cur) => change(acc,cur,op2));
+        let candidate2 = expression.split(op1).map(el => {
+            return el.split(op2).map(Number).reduce((acc,cur) => change(acc,cur,op2));
+        }).reduce((acc,cur) => change(acc,cur,op1));
+        return Math.max(Math.abs(candidate1),Math.abs(candidate2));
+    }else{
+        let operator_list = ['*+-', '*-+', '+*-', '+-*', '-*+', '-+*'];
+        let num_list = expression.split('+').join(' ').split('-').join(' ').split('*').join(' ').split(' ');
+        let answer_list = [];
+        operator_list.map(el => {
+            let [op1,op2,op3] = el.split('');
+            let candidate = expression.split(op1).map(el => {
+            return el.split(op2).map(el2 => {
+                return el2.split(op3).map(Number).reduce((acc,cur) => change(acc,cur,op3));
+            }).reduce((acc,cur) => change(acc,cur,op2));
+        }).reduce((acc,cur) => change(acc,cur,op1));
+            answer_list.push(Math.abs(candidate));
+        })
+        return Math.max(...answer_list);
+    }
+}
