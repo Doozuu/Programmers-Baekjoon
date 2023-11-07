eb4bedf677cc5c9cacc0d6ddf51f14ad9e6e1dd3
LJH <104717341+Doozuu@users.noreply.github.com>
2023-11-07 16:58:01 +0900

    [Gold IV] Title: 단어 수학, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.js
+++ 백준/Gold/1339. 단어 수학/단어 수학.js
@@ -2,7 +2,7 @@ const input = require("fs").readFileSync("dev/stdin").toString().split("\n");
 const [N, ...list] = input;
 
 const map = new Map();
-let number = 9;
+let number = 10;
 
 list.map((item) =>
   [...item].map((a, idx) => {
@@ -12,18 +12,10 @@ list.map((item) =>
 );
 
 const mapEntries = Array.from(map);
-mapEntries
-  .sort((a, b) => b[1] - a[1])
-  .map((el) => {
-    el[1] = number;
-    number--;
-  });
-const sortedMap = new Map(mapEntries);
+let sortedMap = mapEntries.sort((a, b) => b[1] - a[1]);
+let answer = sortedMap.reduce((acc, cur) => {
+  number--;
+  return acc + cur[1] * number;
+}, 0);
 
-let sum = 0;
-let transform = list.map((el) =>
-  [...el].reduce((acc, cur) => acc + sortedMap.get(cur), "")
-);
-sum = transform.reduce((acc, cur) => +acc + +cur, 0);
-
-console.log(sum);
+console.log(answer);
