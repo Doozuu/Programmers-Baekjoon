8c2d89d9fa9faf613941e01b972d64df0aa29815
Doozuu (Julie) <104717341+Doozuu@users.noreply.github.com>
2025-09-09 16:27:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 164 ms, Memory: 10316 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,74 +1,34 @@
-const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt").toString().trim().split('\n');
+const filePath = process.platform === "linux" ? "dev/stdin" : "../test.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const N = +input[0];
-const arr = input[1].split(' ').map(Number);
-const op_arr = [];
+function solution(input) {
+  const N = input[0].split(" ").map(Number)[0];
+  const arr = input[1].split(" ").map(Number);
+  const operators = input[2].split(" ").map(Number);
+  let max = -Infinity;
+  let min = Infinity;
 
-input[2].split(' ').map((el,i) => {
-  if(i === 0){
-    const r = '+'.repeat(el).split('');
-    op_arr.push(...r);
-  }else if(i === 1){
-    const r = '-'.repeat(el);
-    op_arr.push(...r);
-  }else if(i === 2){
-    const r = '*'.repeat(el);
-    op_arr.push(...r);
-  }else if(i === 3){
-    const r = '/'.repeat(el);
-    op_arr.push(...r);
-  }
-});
+  const calculate = [(a, b) => a + b, (a, b) => a - b, (a, b) => a * b, (a, b) => ~~(a / b)];
 
-function solution(){
-  const operations = [];
-  const visited = Array.from({length: op_arr.length}, el => false);
-
-  function dfs(curr, visited){
-    if(curr.length === op_arr.length){
-      operations.push(curr.join(' '));
+  const dfs = (count, result) => {
+    if (count === N - 1) {
+      max = Math.max(max, result);
+      min = Math.min(min, result);
       return;
-    }
-
-    for(let i=0;i<op_arr.length;i++){
-      if(!visited[i]){
-        visited[i] = true;
-        curr.push(op_arr[i]);
-        dfs(curr, visited);
-        visited[i] = false;
-        curr.pop();
-      }
-    }
-  }
-
-  dfs([], visited);
-
-  const set = new Set(operations);
-  const filteredOperation = [...set];
-  const result = [];
-
-  for(let i=0;i<filteredOperation.length;i++){
-    const op = filteredOperation[i].split(' ');
-    let sum = arr[0];
-    for(let j=1;j<N;j++){
-      if(op[j - 1] === '+'){
-        sum += arr[j];
-      }else if(op[j - 1] === '-'){
-        sum -= arr[j];
-      }else if(op[j - 1] === '*'){
-        sum *= arr[j];
-      }else if(op[j - 1] === '/'){
-        if(sum > 0){
-          sum = Math.floor(sum / arr[j]);
-        }else{
-          sum = -Math.floor(-sum / arr[j]); 
+    } else {
+      for (let i = 0; i < 4; i++) {
+        if (!operators[i]) {
+          continue;
         }
+        operators[i]--;
+        dfs(count + 1, calculate[i](result, arr[count + 1]));
+        operators[i]++;
       }
     }
-    result.push(sum);
-  }
+  };
 
-  console.log([Math.max(...result), Math.min(...result)].join('\n'))
+  dfs(0, arr[0]);
+  return max + `\n` + min;
 }
 
-solution();
+console.log(solution(input));
