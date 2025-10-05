c82a553d49fab85ff5313c0dd0a4a6335e8a0b9c
Doozuu (Julie) <104717341+Doozuu@users.noreply.github.com>
2025-10-06 02:00:59 +0900

    [level 2] Title: [3차] n진수 게임, Time: 36.52 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,15 +1,11 @@
 function solution(n, t, m, p) {
     let numbers = '';
     let num = 0;
-    let maxLen = m*t;
     
-    while(numbers.length < maxLen){
-        const new_num = num.toString(n);
-        numbers += new_num;
+    while(numbers.length < m*t){
+        numbers += num.toString(n);
         num++;
     }
     
-    const answer = numbers.slice(0,m*t).split('').filter((_,i) => i%m === p-1);
-    
-    return answer.join('').toUpperCase();
+    return numbers.slice(0,m*t).split('').filter((_,i) => i%m === p-1).join('').toUpperCase();
 }
