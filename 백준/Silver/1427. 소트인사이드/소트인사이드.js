dcfade6f8a46f8899aef438d6a4020e8043abb95
Doozuu (Julie) <104717341+Doozuu@users.noreply.github.com>
2025-11-13 23:42:56 +0900

    [Silver V] Title: 소트인사이드, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : './test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+function solution(){
+ console.log(input[0].split('').map(Number).sort((a,b) => b-a).join(''))
+}
+
+solution();
