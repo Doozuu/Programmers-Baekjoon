353140b7e6dd47f310cbec51394f41aab2bf8559
Doozuu (Julie) <104717341+Doozuu@users.noreply.github.com>
2026-03-20 18:15:37 +0900

    [level 3] Title: 등굣길, Time: 0.97 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,28 +1,24 @@
 function solution(m, n, puddles) {
-    let answer = 0;
-    const graph = Array.from({length: n}, () => Array(m).fill(-1));
+    const dp = Array.from({length: n}, () => Array(m).fill(-1));
     const MOD = 1000000007;
-    const move = [[0,1],[1,0]];
-
-    graph[0][0] = 1;
+    
+    dp[0][0] = 1;
     
     for(let [px,py] of puddles){
-        graph[py-1][px-1] = 0
+        dp[py-1][px-1] = 0;
     }
 
     for(let i=0;i<n;i++){
         for(let j=0;j<m;j++){
             if(i === 0 && j === 0) continue; // 초기 좌표 패스
-            if(graph[i][j] === 0) continue; // 물이 잠긴 지역 패스
-            if(i-1 < 0){
-                graph[i][j] = graph[i][j-1];
-            }else if(j-1 < 0){
-                graph[i][j] = graph[i-1][j];
-            }else{
-                graph[i][j] = (graph[i-1][j] + graph[i][j-1]) % MOD;    
-            }
+            if(dp[i][j] === 0) continue; // 물이 잠긴 지역 패스
+           
+            const top = i > 0 ? dp[i-1][j] : 0;
+            const left = j > 0 ? dp[i][j-1] : 0;
+         
+            dp[i][j] = (left + top) % MOD;    
         }
     }
    
-    return graph[n-1][m-1]
+    return dp[n-1][m-1]
 }
