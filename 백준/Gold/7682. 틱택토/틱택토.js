c3c00b77a9cbf5935b50b9d816c4525f88fb55e1
Doozuu (Julie) <104717341+Doozuu@users.noreply.github.com>
2026-04-19 19:24:55 +0900

    [Gold V] Title: 틱택토, Time: 100 ms, Memory: 9744 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.js
+++ 백준/Gold/7682. 틱택토/틱택토.js
@@ -1,36 +1,80 @@
-const fs = require('fs');
-const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : './test.txt').toString().trim().split('\n');
-const bingo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-const answer = [];
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+let idx = 0;
 
-function isValid(testcase){
-  const testArr = [...testcase];
-  let Xcount = testArr.filter(t => t === "X").length
-  let Ocount = 9 - Xcount - testArr.filter(t => t === '.').length;
+function count(str) {
+  const count_arr = [0, 0, 0];
 
-  let Xwin = false;
-  let Owin = false;
+  for (let i = 0; i < 9; i++) {
+    if (str[i] === "X") count_arr[0]++;
+    if (str[i] === "O") count_arr[1]++;
+    if (str[i] === ".") count_arr[2]++;
+  }
 
-  for(let [i1,i2,i3] of bingo){
-    if(testcase[i1] === testcase[i2] && testcase[i2] === testcase[i3] && testcase[i1] !== '.'){
-      if(testcase[i1] === "X"){
-        Xwin = true;
-      }else{
-        Owin = true;
-      }
+  return count_arr;
+}
+
+function calcIsFinish(str) {
+  const possibleFinish = [
+    "012",
+    "345",
+    "678",
+    "036",
+    "147",
+    "258",
+    "048",
+    "246",
+  ];
+
+  let xWin = false,
+    oWin = false;
+
+  for (let i = 0; i < possibleFinish.length; i++) {
+    const [a, b, c] = possibleFinish[i].split("").map(Number);
+
+    if (str[a] === str[b] && str[b] === str[c] && str[a] !== ".") {
+      if (str[a] === "X") xWin = true;
+      if (str[a] === "O") oWin = true;
     }
   }
 
-  if(Xcount < Ocount || Xcount > Ocount + 1) return "invalid"
-  if(Xwin && Owin) return "invalid"
-  if(Xwin && Xcount !== Ocount + 1) return "invalid"
-  if(Owin && Xcount !== Ocount) return "invalid"
-  if(!Xwin && !Owin && Xcount + Ocount !== 9) return "invalid"
-  return "valid"
+  if (xWin && oWin) return [true, "BOTH"];
+  if (xWin) return [true, "X"];
+  if (oWin) return [true, "O"];
+  return [false, ""];
 }
 
-for(let testcase of input){
-  if(testcase !== 'end') answer.push(isValid(testcase))
-}
+while (true) {
+  if (input[idx] === "end") break;
 
-console.log(answer.join('\n'))
+  let answer = "valid";
+  const testcase = input[idx++];
+  const [xCount, oCount, emptyCount] = count(testcase);
+
+  // 규칙 1
+  if (oCount > xCount) {
+    console.log("invalid");
+    continue;
+  }
+
+  const [isFinish, finishC] = calcIsFinish(testcase);
+
+  if (isFinish) {
+    // 규칙 2
+    if (
+      finishC === "BOTH" ||
+      (finishC === "X" && xCount !== oCount + 1) ||
+      (finishC === "O" && xCount !== oCount)
+    )
+      answer = "invalid";
+  } else {
+    // 규칙 3
+    if (emptyCount > 0) answer = "invalid";
+  }
+
+  console.log(answer);
+}
